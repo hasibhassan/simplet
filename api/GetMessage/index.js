1b86d3552f4cd1918a939b8a56caf0bab0858155
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-05-06 09:49:49 -0500

    fix: api

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -3,9 +3,10 @@ module.exports = async function (context, req) {
     'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=4'
   )
   const data = apiResponse
+  console.log(data)
 
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: data,
+    body: 'Hello World',
   }
 }
