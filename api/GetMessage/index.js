239d62962d2b2d2cf2804fadfbbf83cc6cda08b7
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-05-06 09:38:52 -0500

    fix: api

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
   const apiResponse = await fetch(
     'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=4'
   )
-  const data = await apiResponse.json()
+  const data = apiResponse
 
   context.res = {
     // status: 200, /* Defaults to 200 */
