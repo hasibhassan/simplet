2b6f704587b852309edf9a4916347cd9c274a3d9
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-05-06 09:34:58 -0500

    fix: api

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -2,9 +2,7 @@ module.exports = async function (context, req) {
   const apiResponse = await fetch(
     'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=4'
   )
-  const data = apiResponse
-    ? apiResponse
-    : 'This HTTP triggered function unexpectedly failed.'
+  const data = await apiResponse.json()
 
   context.res = {
     // status: 200, /* Defaults to 200 */
