b17bce49c55a3ca30a26087d16ca5de54a175a95
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-05-07 01:49:00 -0500

    fix: azure functions

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,24 +1,28 @@
 const axios = require('axios').default
 
 module.exports = async function (context, req) {
-  async function getUser() {
-    try {
-      const response = await axios.get(
-        'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=4'
-      )
-      const apiData = await response.json()
-      return apiData
-    } catch (error) {
-      console.error(error)
-      return 'something went wrong'
-    }
-  }
-  const data = getUser()
+  // async function getUser() {
+  //   try {
+  //     const response = await axios.get(
+  //       'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=4'
+  //     )
+  //     const apiData = await response.json()
+  //     return apiData
+  //   } catch (error) {
+  //     console.error(error)
+  //     return 'something went wrong'
+  //   }
+  // }
+  // const data = getUser()
+  // return {
+  //   status: 200,
+  //   body: data,
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  // }
+
   return {
-    status: 200,
-    body: data,
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    body: context.executionContext.invocationId,
   }
 }
