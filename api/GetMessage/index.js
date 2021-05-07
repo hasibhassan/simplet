ada74dea2ccc60e09d2cf52546677159323b7dd6
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-05-07 02:13:27 -0500

    fix: azure functions api

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,17 +1,8 @@
 const axios = require('axios')
 
 module.exports = async function (context, req) {
-  async function getUser() {
-    try {
-      const response = await axios.get(
-        'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=4'
-      )
-      return response
-    } catch (error) {
-      console.error(error)
-      return 'something went wrong'
-    }
-  }
-  const data = getUser()
-  context.res.json(data)
+  const response = await axios.get(
+    'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=4'
+  )
+  context.res.json(response)
 }
