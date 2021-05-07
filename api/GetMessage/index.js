7b71adb6d0deec9538c37129c56b3d118ad29c51
hqhas <67892792+hasibhassan@users.noreply.github.com>
2021-05-07 01:20:00 -0500

    fix: api

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,12 +1,15 @@
 module.exports = async function (context, req) {
-  // const apiResponse = await fetch(
-  //   'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=4'
-  // )
-  // const data = apiResponse
-  // console.log(data)
+  const apiResponse = await fetch(
+    'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=4'
+  )
+  const data = apiResponse
+  console.log(data)
 
-  context.res = {
+  return {
     // status: 200, /* Defaults to 200 */
-    body: 'hello world',
+    body: data,
+    headers: {
+      'Content-Type': 'application/json',
+    },
   }
 }
